524048333e945efc822e98205eda02274de83adc
Franzi <franziska.moenster@sap.com>
2016-06-17 14:04:03 +0200

    first GET Frontend

--- src/Frontend/www/js/controllers.js
+++ src/Frontend/www/js/controllers.js
@@ -1,77 +1,94 @@
+//import {Http} from "angular/http";
 angular.module('app.controllers', [])
-  
-.controller('homeTabCtrl', function($scope) {
 
+.controller('homeTabCtrl', function($scope, $http) {
+  $scope.anzeigenByID=function(id) {
+    //alert("Geklickt");
+    $http.get('http://localhost:3000/api/smarthandwerk/angebot/angeboterstellen?id=5').success(function (response) {
+      //body der function um erfolgmeldungen abzuarbeiten
+      /*  if(!error && response.statusCode==200){
+       //erfolgreich
+       alert("Erfolgreich angezeigt")
+       }
+       else{
+       //nicht erfolgreich
+       alert("Fehler: " + response.statusCode); //hier noch internen Fehlercode
+       }
+       })
+       //hier noch generisch ändern!*/
+      console.log(response);
+    })
+  }
 })
-   
+
 .controller('nachrichtenCtrl', function($scope) {
 
 })
-   
+
 .controller('mapsCtrl', function($scope, $state, $cordovaGeolocation) {
   var options = {timeout: 10000, enableHighAccuracy: true};
- 
+
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
- 
+
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
+
     var mapOptions = {
       center: latLng,
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
- 
+
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
- 
+
   }, function(error){
     console.log("Could not get location");
   });
 })
-         
+
 .controller('loginCtrl', function($scope) {
 
 })
-   
+
 .controller('signupCtrl', function($scope) {
 
 })
-   
+
 .controller('anlegenCtrl', function($scope) {
 
 })
-   
+
 .controller('meineOrteCtrl', function($scope) {
 
 })
-   
+
 .controller('meineBeitrGeCtrl', function($scope) {
 
 })
-   
+
 .controller('historieCtrl', function($scope) {
 
 })
-   
+
 .controller('einstellungenCtrl', function($scope) {
 
 })
-   
+
 .controller('hilfeCtrl', function($scope) {
 
 })
-   
+
 .controller('profilCtrl', function($scope) {
 
 })
-   
+
 .controller('anfrageBersichtCtrl', function($scope) {
 
 })
-   
+
 .controller('anfragenannahmeCtrl', function($scope) {
 
 })
-   
+
 .controller('anfrageErstellenCtrl', function($scope, $http) {
     $http.get('ElementeAnfrage.json').then(function(elementsResponse) {
       $scope.kategorien = elementsResponse.data;
@@ -84,24 +101,23 @@ angular.module('app.controllers', [])
         $scope.myVar = !$scope.myVar;
     };
  })
-   
+
 .controller('angebotsBersichtCtrl', function($scope) {
 
 })
-   
+
 .controller('anfrageErstellenBersichtCtrl', function($scope) {
 
 })
-   
+
 .controller('paketeCtrl', function($scope) {
 
 })
-   
+
 .controller('chatEinzelpersonCtrl', function($scope) {
 
 })
-   
+
 .controller('chatGruppenchatCtrl', function($scope) {
 
 })
- 
